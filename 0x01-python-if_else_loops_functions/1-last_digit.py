a727629e1e883b434293c2c73312989e359a2a33
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-06-06 13:07:54 -0700

    Fix output nigative Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1])
+last_digit = int(str(number)[-1]) if number > 0 else int(str(number)[-1]) * -1
+
 msg = f"Last digit of {number} is {last_digit}"
 if last_digit > 5:
     msg += " and is greater than 5"
