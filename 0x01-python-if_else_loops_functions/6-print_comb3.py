4591e710fc3511d63f64d365a609ef045659982e
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-06-06 19:54:50 -0700

    Fix task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for num in range(1, 90):
-    print("{:02d}".format(num), end=', ' if num < 89 else '\n')
+for i in range(0, 9):
+    for j in range(i+1, 10):
+        print("{:d}{:d}".format(i, j), end=', ' if i < 8 else '\n')
