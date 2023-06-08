9249cfb016d3bc9261c81134087828e0d677a7a5
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-06-08 14:40:29 -0700

    Fix Task -8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,8 +3,8 @@ def magic_calculation(a, b):
     add = __import__('magic_calculation_102').add
     sub = __import__('magic_calculation_102').sub
     if a < b:
-        c = a + b
-        for i in range(98):
+        c = add(a, b)
+        for i in range(4, 6):
             c = add(c, i)
         return c
     else:
