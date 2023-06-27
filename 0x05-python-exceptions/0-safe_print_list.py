9d1f221887ff0b1958c5e75cca1fb3d3d5d1f085
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-06-26 17:36:39 -0700

    Fix - local variable 'i' referenced before assignment

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    counter = 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
+            counter += 1
     except IndexError:
         pass
-    else:
-        i += 1
     print("")
-    return i
+    return counter
