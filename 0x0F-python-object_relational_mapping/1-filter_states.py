377d2e20d1a63ea6dcfe86fddda102d09d2caa5c
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-11-17 07:55:29 -0800

    0x0F- Task 1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa."""
+""" Script that lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa."""
+
 import MySQLdb
 import sys
 
@@ -8,7 +10,8 @@ def print_states(argv):
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states""")
+    cursor.execute("""SELECT * FROM states
+            WHERE name LIKE 'N%' ORDER BY states.id""")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
