3994c5f8a8d80544aec0712431d861b05ea21768
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-12-06 03:49:42 +0100

    0x11. Task 0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as req:
+        response = req.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode('UTF-8')))
